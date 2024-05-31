0d1a2cba223d4bd53997abc9608c8b073876ab1f
Yash Gillorkar <144929163+YashGillorkar@users.noreply.github.com>
2024-05-31 10:28:11 +0530

    Delete method committed

--- SchoolManagement/src/main/java/com/cjc/main/controller/HomeController.java
+++ SchoolManagement/src/main/java/com/cjc/main/controller/HomeController.java
@@ -1,9 +1,15 @@
 package com.cjc.main.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 	
 
+	@DeleteMapping("/delete")
+	public String deleteReqest() {
+		System.out.println("deleteRequest");
+		return "delete By Yash";
+	}
 }

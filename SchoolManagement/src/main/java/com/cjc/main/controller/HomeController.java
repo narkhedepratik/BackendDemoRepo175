f3994aa5aae5827cef0ff4a581924efe12de7d66
shwetadhopre <shwetadhopre@gmail.com>
2024-05-31 10:30:50 +0530

    done

--- SchoolManagement/src/main/java/com/cjc/main/controller/HomeController.java
+++ SchoolManagement/src/main/java/com/cjc/main/controller/HomeController.java
@@ -4,6 +4,7 @@ import java.lang.annotation.Target;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
@@ -16,7 +17,12 @@ public class HomeController {
 		
 		return "Get the Data Successfully!..";
 	}
-	
+	@PostMapping("/insert")
+	public String post()
+	{
+		System.out.println("post mapping");
+		return "successful";
+	}
 	
 
 }

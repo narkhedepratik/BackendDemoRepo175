1d979db9756885c9b1cfdf0786775164f1488024
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2024-05-31 10:22:49 +0530

    get method committed

--- SchoolManagement/src/main/java/com/cjc/main/controller/HomeController.java
+++ SchoolManagement/src/main/java/com/cjc/main/controller/HomeController.java
@@ -1,8 +1,22 @@
 package com.cjc.main.controller;
 
+import java.lang.annotation.Target;
+
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 public class HomeController {
+	
+	@GetMapping("/path")
+	public String getMethodName() {
+		
+		System.out.println("Get Mapping mapi..");
+		
+		return "Get the Data Successfully!..";
+	}
+	
+	
 
 }
